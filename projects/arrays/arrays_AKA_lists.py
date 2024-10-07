1fcaf724c99aea059a5e6578d10bc9a38a467348
Dylan <dylannelson58@gmail.com>
2024-10-07 13:12:52 -0700

    Working through characteristics of lists / arrays.

---
+++ projects/arrays/arrays_AKA_lists.py
@@ -0,0 +1,16 @@
+# Dylan Nelson 
+# October 07, 2024
+# arrays_AKA_lists.py
+
+mylist = [1,2,3,4,5]
+
+# The direct index that we are looking for can be called in an array.
+print(mylist[0])
+print(mylist[2])
+
+# The entire array / list can be printed.
+print(mylist)
+
+# Using a for loop the contents of an array / list can be printed
+for i in range(len(mylist)):
+    print(mylist[i])
