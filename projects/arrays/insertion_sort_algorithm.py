7d82091d8ad58d5f3ab8aa92ffff5e90817a0da7
Dylan <dylannelson58@gmail.com>
2024-10-11 10:34:07 -0700

    Fixed the issues with the sorting algorithm, removed debugging print statements. Added in context comments.

--- projects/arrays/insertion_sort_algorithm.py
+++ projects/arrays/insertion_sort_algorithm.py
@@ -12,26 +12,38 @@ def insertion_sort_algorithm():
     unsorted_list = [43, 21, 12, 45, 67, 23, 76, 14, 86, 13]
     print(f'Unsorted list contents: {unsorted_list}')
 
-    iterationCount = len(unsorted_list)
+    # Copy the unsorted list into a list that we will modify.
+    modified_list = unsorted_list
+    # Create a variable that represents the selected index of the list.
+    iterationCount = len(modified_list) - 1
     print(f"The length of the list is: {iterationCount}")
-    for i in range(iterationCount):
-        if i < iterationCount - 1:
-            print(f"We are on loop {i}")
-            print(f"The item at index {i} is {unsorted_list[i]}")
-            print(f"The item at index {i} + 1 is {unsorted_list[i+1]}")
-            goodToContinue = True
-        if i >= iterationCount - 1:
-            goodToContinue = False
-        elif goodToContinue and (unsorted_list[i] > unsorted_list[i+1]):
-            storedVariable = unsorted_list[i+1]
-            unsorted_list[i+1] = unsorted_list[i]
-            unsorted_list[i] = storedVariable
-        elif goodToContinue and (unsorted_list[i] < unsorted_list[i+1]):
-            storedVariable = unsorted_list[i]
-            unsorted_list[i] = unsorted_list[i+1]
-            unsorted_list[i+1] = storedVariable
-    
-    print(f'Sorted list contents: {unsorted_list}')
+    for t in range(iterationCount):
+        for i in range(iterationCount):
+            # Create a boolean flag to indicate whether the index will be in 
+            #   bounds for the array. 
+            if i < iterationCount:
+                goodToContinue = True
+            # The flag is flase if the index is greater than or equal to the 
+            #   iterationCount, this is because we cannot call a subscript / 
+            #   index that is larger than the array we are looking at.
+            if i >= iterationCount:
+                goodToContinue = False
+            # Here we check if we are good to continue and if the index value 
+            #   is greater than the index value plus 1.
+            if goodToContinue and (modified_list[i] > modified_list[i+1]):
+                # Store the value of the higher index in a temporary variable.
+                storedVariable = modified_list[i+1]
+                # Replace the location of the higher index number with the 
+                #   number that is numerically greater than it.
+                modified_list[i+1] = modified_list[i]
+                # Use the stored variable to replace the value that was less 
+                #   numerically less than it.
+                modified_list[i] = storedVariable
+        # Print the list to show the progress that is being made.
+        print(modified_list)
+
+    sorted_list = modified_list
+    print(f'Sorted list contents: {sorted_list}')
             
 if __name__ == '__main__':
     insertion_sort_algorithm()
