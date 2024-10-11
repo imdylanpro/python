eabbdef02072b280778787b610300e49b89c5d01
Dylan <dylannelson58@gmail.com>
2024-10-11 11:11:48 -0700

    Demonstrating characteristics of types.

---
+++ projects/data_types/practicing_data_types.py
@@ -0,0 +1,13 @@
+# Dylan Nelson
+# October 11, 2024
+# practicing_data_types.py
+
+string_list = ["January", "February", "March", "April", "May", "June", "July",
+               "August", "September", "October", "November", "December"]
+int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# The type reports back the tpe of the object we call. The type of a list is 
+#   list. The type of an individual element of a list depends on the contents.
+print(type(string_list))
+print(type(string_list[0]))
+print(type(int_list[0]))
