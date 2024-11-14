a350531954ba42f7a0cb94d305c9b2ad4d0ab03f
Dylan <dylannelson58@gmail.com>
2024-11-14 08:44:03 -0800

    A basic program that creats ASCII art of a mouse.

---
+++ projects/Basic_Programming/mouse_art.py
@@ -0,0 +1,12 @@
+# Dylan Nelson
+# November 14, 2024
+# mouse_art.py
+
+'''Prints some artwork of a mouse.'''
+
+# Note that the backslash needs two chars to output one backslash.
+print('         (\\-.')
+print('         /  _`>')
+print(' _)     /  _)=')
+print('(      /  _/')
+print(' `-.__(___)_')
