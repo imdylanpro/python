89006b67cd9be0551705bbf688ab9ad284899361
Dylan <dylannelson58@gmail.com>
2024-11-26 16:52:49 -0800

    Demonstrating using relational operators simply, and showing how to use if statements in one line.

---
+++ projects/Basic_Programming/Module_3/relational_operators.py
@@ -0,0 +1,16 @@
+# Dylan Nelson
+# November 26, 2024
+# relational_operators.py
+
+a = 1
+b = 2
+
+print("a is", a, "b is", b)
+print("a is greater than b.") if a < b else print("a is not greater than b.")
+print("a is less than b.") if a > b else print("a is not less than b.")
+print("a is greater than or equal to b.") if a > b else print("a is not "
+    "greater than or equal to b.")
+print("a is less than or equal to b.") if a > b else print("a is not "
+    "less than or equal to b.")
+print("a is equal to b.") if a == b else print("a is not equal to b.")
+print("a is not equal to b.") if a != b else print("a is equal to b.")
